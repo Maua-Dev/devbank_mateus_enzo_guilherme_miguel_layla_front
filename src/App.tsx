a25c3848d721daf203fb71febca35a9ae68cd48b
Matelz <49626198+Matelz@users.noreply.github.com>
2025-04-28 20:44:01 -0300

    feat: added tailwind and example page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
-import { useState } from 'react'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>🚀 Vite React Template 🚀</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
+    <div className='flex flex-col items-center justify-center h-screen bg-gradient-to-br gap-y-4 text-white from-blue-500 to-purple-500'>
+      <h1 className="text-5xl font-bold underline">
+        DevBank! 🚀
+      </h1>
+      <p className="animate-pulse">
+        Em desenvolvimento... 👀
+      </p>
+    </div>
   )
 }
 
